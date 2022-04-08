547b1e2798653d1d232ff6f932e47d316dcbf3f5
NGUYENHAMANH <linhmanhngan@gmail.com>
2022-04-08 11:09:52 +0700

    BTVN buoi3

---
+++ BTVN/BTVN 4.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	//input n
+	int n;
+	printf("input n: ");
+	scanf("%d",&n);
+	//a=0
+	int a=0;
+	 while (n>0){
+	 	a=a*10+n%10;
+	 	n=n/10;
+	 }
+	 printf("so dao nguoc cua so da nhap la %d",a);
+
+}
+
